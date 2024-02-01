4778bf0bad01747cd73811b64d86929b1c54933d
sadishaaaa <sthasadisha2019@gmail.com>
2024-02-01 21:42:59 +0545

    server crud completed

--- server/src/controller/taskController.ts
+++ server/src/controller/taskController.ts
@@ -17,3 +17,67 @@ export const createTask = async (req: Request, res: Response) => {
     });
   }
 };
+export const getAllTask = async (req: Request, res: Response) => {
+  try {
+    let result = await taskModel.find({});
+    res.json({
+      success: true,
+      message: " all task displayed sucessfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const getTaskById = async (req: Request, res: Response) => {
+  let id = req.params.id;
+  try {
+    let result = await taskModel.findById(id);
+    res.json({
+      success: true,
+      message: "  task displayed sucessfully acc to id",
+      data: result,
+    });
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const updateTaskById = async (req: Request, res: Response) => {
+  let id = req.params.id;
+  let data = req.body;
+  try {
+    let result = await taskModel.findByIdAndUpdate(id, data, { new: true });
+    res.json({
+      success: true,
+      message: "Teacher patch sucessfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const deleteTaskByID = async (req: Request, res: Response) => {
+  let id = req.params.id;
+  try {
+    let result = await taskModel.findByIdAndDelete(id);
+    res.json({
+      success: true,
+      message: "deleted sucessfully",
+    });
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server/src/route/taskRoute.ts
+++ server/src/route/taskRoute.ts
@@ -1,8 +1,19 @@
 import { Router, Request, Response } from "express";
-import { createTask } from "../controller/taskController";
+import {
+  createTask,
+  deleteTaskByID,
+  getAllTask,
+  getTaskById,
+  updateTaskById,
+} from "../controller/taskController";
 
 let taskRouter = Router();
 
-taskRouter.route("/").post(createTask);
+taskRouter.route("/").post(createTask).get(getAllTask);
+taskRouter
+  .route("/:id")
+  .get(getTaskById)
+  .patch(updateTaskById)
+  .delete(deleteTaskByID);
 
 export default taskRouter;
